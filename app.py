b54099d32ee69eba61231fd4c4d4ebeec7a30aea
akash485 <66115637+akash485@users.noreply.github.com>
2024-12-05 12:53:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Dockerized Flask App!'
+def hello_cloud():
+    return 'Hello from akash ECS Container'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
