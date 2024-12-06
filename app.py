d4d5eb1cd9e91cdfb757ddb2bb3ace2382ad73ce
akash485 <66115637+akash485@users.noreply.github.com>
2024-12-05 19:41:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-    return 'Hello from akash ECS Container'
+    return 'Hello from Andekar ECS Container'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
